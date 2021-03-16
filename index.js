6e88b85b332d736e7033dd4a9ff2b8ef0a478097
aiden927 <aiden927@users.noreply.github.com>
2021-03-15 22:19:32 -0400

    set up prod interfaces

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./.config/keys');
 
 require('./models/User');
@@ -25,6 +27,18 @@ connectDB();
 
 //authRoutes
 const app = express();
+
+//middlewares
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
 require('./routes/authRoutes')(app);
 
 
